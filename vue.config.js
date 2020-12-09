47ea0bfaccc52a6fe8ea949849699677226d7f6d
lzjlxebr <tyyzmyd@gmail.com>
2020-12-10 01:16:24 +0800

    Cancel compressing

--- vue.config.js
+++ vue.config.js
@@ -87,23 +87,6 @@ module.exports = {
       )
 
     config
-      .when(process.env.NODE_ENV === 'production',
-        config => {
-          config
-            .plugin('gzip-plugin')
-            .use('compression-webpack-plugin', [{
-              filename: '[path].gz[query]',
-              algorithm: 'gzip',
-              test: /\.js$|\.json$|\.css$|\.ttf$|\.eot$|\.woff$|\.woff2$/,
-              threshold: 0, // 只有大小大于该值的资源会被处理
-              minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
-              deleteOriginalAssets: true // 删除原文件
-            }])
-            .end()
-        }
-      )
-
-    config
       .when(process.env.NODE_ENV !== 'development',
         config => {
           config
